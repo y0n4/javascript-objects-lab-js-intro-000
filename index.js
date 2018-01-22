49b485a9716d976794c9642de8dd63641dfb2e7c
yona l <04l.yona@gmail.com>
2018-01-22 20:56:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
- var newObj = Object.assign({}, targetObject, updatesObject);
- delete newObj.foo
+  
 }
